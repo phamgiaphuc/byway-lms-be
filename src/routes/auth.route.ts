fe82250af7e1af69a2bec23b73c1ffbad7e1d530
Acus <acuscodinghcm@gmail.com>
2025-11-24 14:18:26 +0700

    Phuc | Add code

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,3 +1,4 @@
+import { ApiResponse } from "@/lib/api-response";
 import { authRoute } from "@/types/routes/auth.route";
 import { Request, Router, Response } from "express";
 import { StatusCodes } from "http-status-codes";
@@ -5,10 +6,7 @@ import { StatusCodes } from "http-status-codes";
 const router = Router();
 
 router.get(authRoute.status, (_: Request, res: Response) => {
-  res.status(StatusCodes.OK).json({
-    message: "Auth APIs",
-    status: "success",
-  });
+  res.status(StatusCodes.OK).json(new ApiResponse(StatusCodes.OK, {}, "auth status"));
 });
 
 export const authRoutes = router;
