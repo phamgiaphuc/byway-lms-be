931e7fc407f6a59c3da33ca6cdedefdf3e285e20
Acus <acuscodinghcm@gmail.com>
2025-12-09 20:20:47 +0700

    Phuc | Update request

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,12 @@
+import { UnauthorizedError } from "@/lib/api-error";
 import { userRepository } from "@/repository/user.repository";
 
 class UserService {
   async getUserProfile(id: string) {
     const user = await userRepository.findUserProfileById(id);
+    if (!user) {
+      throw new UnauthorizedError("User not found");
+    }
     return user;
   }
 }
